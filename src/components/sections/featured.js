a6e9b66af8194f8865c98869cc664cbc1df42a9b
David Jeong <yechanjeong@yahoo.com>
2020-05-01 20:30:34 -0400

    remove filter for featured

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -113,12 +113,10 @@ const StyledFeaturedImg = styled(Img)`
   border-radius: ${theme.borderRadius};
   position: relative;
   mix-blend-mode: multiply;
-  filter: grayscale(100%) contrast(1) brightness(90%);
   ${media.tablet`
     object-fit: cover;
     width: auto;
     height: 100%;
-    filter: grayscale(100%) contrast(1) brightness(80%);
   `};
 `;
 const StyledImgContainer = styled.a`
@@ -127,7 +125,6 @@ const StyledImgContainer = styled.a`
   grid-row: 1 / -1;
   position: relative;
   z-index: 1;
-  background-color: ${colors.green};
   border-radius: ${theme.radius + 1}px;
   transition: ${theme.transition};
   ${media.tablet`height: 100%;`};
@@ -155,7 +152,7 @@ const StyledImgContainer = styled.a`
     bottom: 0;
     z-index: 3;
     transition: ${theme.transition};
-    background-color: ${colors.background};
+    background: transparent;
     mix-blend-mode: screen;
   }
 `;
