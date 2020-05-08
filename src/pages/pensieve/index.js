cca19aafa6211ffda9a95958fca1bad7729e7d8c
David Jeong <yechanjeong@yahoo.com>
2020-05-08 01:33:17 -0400

    delete things that are not needed

--- src/pages/pensieve/index.js
+++ src/pages/pensieve/index.js
@@ -123,8 +123,8 @@ const PensievePage = ({ location, data }) => {
   return (
     <Layout location={location}>
       <Helmet>
-        <title>Pensieve | Brittany Chiang</title>
-        <link rel="canonical" href="https://brittanychiang.com/pensieve" />
+        <title>Pensieve | David Jeong</title>
+        <link rel="canonical" href="https://???.com/pensieve" />
       </Helmet>
 
       <StyledMainContainer>
