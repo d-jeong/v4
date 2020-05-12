6032db566df8ea89883d44f0ed26a13942371fd0
David Jeong <yechanjeong@yahoo.com>
2020-05-11 22:42:16 -0400

    change config name

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'DavidJeong',
+        short_name: 'DavidJeong',
         start_url: '/',
         background_color: config.colors.background,
         theme_color: config.colors.background,
